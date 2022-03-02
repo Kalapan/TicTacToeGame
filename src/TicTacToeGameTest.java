446d3eb4a70bedcf0abceb893c6728bdf3e8754a
Kalapan Kannathasan <kalapank@hotmail.com>
2022-03-01 20:14:53 -0500

    uploaded tic tac toe game

---
+++ src/TicTacToeGameTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertEquals;
+
+public class TicTacToeGameTest {
+
+    @Test
+    public void testWinner() {
+        TicTacToeGame win = new TicTacToeGame();
+        win.checkWinner();
+
+
+    }
+
+}
